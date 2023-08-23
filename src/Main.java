99065753d0c412e89a0ba045af5eed2299ef59d2
TpyMuxa <tpymuxa1133@gmail.com>
2023-08-23 23:38:54 +0500

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
+        byte variableByte = 50;
+        short variableShort = 15000;
+        int variableInteger = 1900501;
+        long variableLong = 1905340999L;
+        float variableFloat = 52.71F;
+        double variableDouble = 25.65691;
+        System.out.println("Значение переменной variableByte с типом byte равно " + variableByte);
+        System.out.println("Значение переменной variableShort с типом short равно " + variableShort);
+        System.out.println("Значение переменной variableInteger с типом int равно " + variableInteger);
+        System.out.println("Значение переменной variableLong с типом long равно " + variableLong);
+        System.out.println("Значение переменной variableFloat с типом float равно " + variableFloat);
+        System.out.println("Значение переменной variableDouble с типом double равно " + variableDouble);
+
     }
 }
